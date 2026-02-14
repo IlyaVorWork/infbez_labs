5b6aa5ca5db61404eb1a31aa70d629de79bf78ea
Aleksandr1331 <asf47@tpu.ru>
2026-02-14 23:08:06 +0700

    feat: Дописана функция Hash по губчатой архитектуре refactor: Update Sponge implementation with new inner state structure and enhance functionality

--- Lab2/SpongeBobSquareHash.go
+++ Lab2/SpongeBobSquareHash.go
@@ -2,14 +2,32 @@ package Lab2
 
 import (
 	l1 "infbez_labs/Lab1"
+	"strings"
+)
+
+var (
+	cBlockInnerState = [4]string{
+		"________________",
+		"ПРОЖЕКТОР_ЧЕПУХИ",
+		"КОЛЫХАТЬ_ПАРОДИЮ",
+		"КАРМАННЫЙ_АТАМАН",
+	}
+
+	SpongeInnerState = [5][5]string{
+		{"____", "____", "____", "____", "____"},
+		{"____", "____", "____", "____", "____"},
+		{"____", "____", "____", "____", "____"},
+		{"____", "____", "____", "____", "____"},
+		{"____", "____", "____", "____", "____"},
+	}
 )
 
 type Sponge struct {
-	InnerState [4]string
+	InnerState [5][5]string
 	Alphabet   l1.Alphabet
 }
 
-func NewSponge(InnerState [4]string, Alphabet l1.Alphabet) *Sponge {
+func NewSponge(InnerState [5][5]string, Alphabet l1.Alphabet) *Sponge {
 	return &Sponge{InnerState, Alphabet}
 }
 
@@ -25,7 +43,7 @@ func (s *Sponge) CoreTrithemius(inPrime, inAux string) string {
 func (s *Sponge) CBlock(inArr []string, outSize int) string {
 	var (
 		CountRow   = len(inArr)
-		innerState = s.InnerState
+		innerState = cBlockInnerState
 	)
 	for i := 0; i < CountRow; i++ {
 		if len([]rune(inArr[i])) == 16 {
@@ -82,4 +100,130 @@ func (s *Sponge) Compress(In16 string, outN int) string {
 	} else {
 		panic("недопустимый размер выходных данных")
 	}
+}
+
+func (s *Sponge) MixCols() [5][5]string {
+	var (
+		X [5]string
+	)
+	for i := 0; i < 5; i++ {
+		X[i] = "____"
+		for j := 0; j < 5; j++ {
+			X[i] = s.Alphabet.AddTxt(X[i], s.InnerState[j][i])
+		}
+		q := (i + 1) % 5
+		for j := 0; j < 5; j++ {
+			tmp := s.Alphabet.AddTxt(X[i], s.InnerState[j][q])
+			s.InnerState[j][q] = s.Alphabet.SubTxt(tmp, s.InnerState[j][i])
+		}
+	}
+	return s.InnerState
+}
+
+//func (s *Sponge) ShiftBlock(block string) string {
+//	blockArr := s.Alphabet.TextToArray(block)
+//	lenBlockArr := len(blockArr)
+//	result := make([]int, lenBlockArr)
+//	for i := 0; i < lenBlockArr; i++ {
+//		result[(i+1)%lenBlockArr] = blockArr[i]
+//	}
+//	return s.Alphabet.ArrayToText(result)
+//}
+
+func (s *Sponge) ShiftBlock(block string) string {
+	blockArr := s.Alphabet.TextToArray(block)
+	shifted := append(blockArr[len(blockArr)-1:], blockArr[:len(blockArr)-1]...)
+	return s.Alphabet.ArrayToText(shifted)
+}
+
+func (s *Sponge) ShatterBlocks() [5][5]string {
+	for i := 0; i < 5; i++ {
+		s.InnerState[i][i] = s.ShiftBlock(s.InnerState[i][i])
+	}
+	return s.InnerState
+}
+
+func (s *Sponge) ShiftRows() [5][5]string {
+	var result [5][5]string
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			q := (j + i) % 5
+			result[j][i] = s.InnerState[q][i]
+		}
+	}
+	s.InnerState = result
+	return result
+}
+
+func (s *Sponge) SpongeAbsorb(inputBlock string) [5][5]string {
+	var columnSums [5]string
+	var str2 = concatStrings([]string{inputBlock, s.InnerState[0][0], inputBlock, s.InnerState[0][0]}, 16)
+
+	for i := 0; i < 5; i++ {
+		columnSums[i] = "____"
+		for j := 0; j < 5; j++ {
+			columnSums[i] = s.Alphabet.AddTxt(columnSums[i], s.InnerState[i][j])
+		}
+	}
+	str1 := concatStrings([]string{columnSums[0], columnSums[1], columnSums[2], columnSums[3]}, 16)
+
+	s.InnerState[0][0] = s.CBlock([]string{str1, str2}, 4)
+	s.MixCols()
+	s.ShatterBlocks()
+	s.ShiftRows()
+	return s.InnerState
+}
+
+func (s *Sponge) SpongeSqueeze() string {
+	s.MixCols()
+	s.ShatterBlocks()
+	s.ShiftRows()
+
+	var columnSums [5]string
+
+	for i := 0; i < 5; i++ {
+		columnSums[i] = "____"
+		for j := 0; j < 5; j++ {
+			columnSums[i] = s.Alphabet.AddTxt(columnSums[i], s.InnerState[i][j])
+		}
+	}
+	str := concatStrings([]string{columnSums[0], columnSums[1], columnSums[2], columnSums[3]}, 16)
+	return s.CBlock([]string{str}, 4)
+}
+
+func SpongeHash(message string, alphabet l1.Alphabet) string {
+	messageLen := len([]rune(message))
+	sponge := NewSponge(SpongeInnerState, alphabet)
+	var builder strings.Builder
+
+	K := 4 - (messageLen % 4)
+	if K < 4 {
+		message = concatStrings([]string{message, strings.Repeat("_", K)}, messageLen+K)
+	}
+
+	messageArr := []rune(message)
+	messageLen = len(messageArr)
+
+	for i := 0; i < messageLen; i += 4 {
+		tpm := messageArr[i : i+4]
+		sponge.SpongeAbsorb(string(tpm))
+	}
+
+	builder.Grow(64)
+	for i := 0; i < 16; i++ {
+		tpm := sponge.SpongeSqueeze()
+		builder.WriteString(tpm)
+	}
+
+	return builder.String()
+}
+
+func concatStrings(strs []string, capacity int) string {
+	var builder strings.Builder
+	builder.Grow(capacity)
+	for _, str := range strs {
+		builder.WriteString(str)
+	}
+	return builder.String()
 }
--- main.go
+++ main.go
@@ -7,26 +7,38 @@ import (
 )
 
 func main() {
-	var (
-		TelegraphAlphabet = []rune("АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ_")
+	var TelegraphAlphabet = []rune("АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ_")
+	var alphabet = *l1.NewAlphabet(TelegraphAlphabet)
+	//var (
+	//	TelegraphAlphabet = []rune("АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ_")
 
-		InnerState = [4]string{"________________",
-			"ПРОЖЕКТОР_ЧЕПУХИ",
-			"КОЛЫХАТЬ_ПАРОДИЮ",
-			"КАРМАННЫЙ_АТАМАН"}
-	)
+	//	state0            = [5][5]string{
+	//		{"____", "____", "____", "____", "____"},
+	//		{"____", "____", "____", "____", "____"},
+	//		{"____", "____", "____", "____", "____"},
+	//		{"____", "____", "____", "____", "____"},
+	//		{"____", "____", "____", "____", "____"},
+	//	}
+	//
+	//	stateX = [5][5]string{
+	//		{"БЫ_Щ", "ЙЖ_Б", "ЮФ_Е", "БЫ_Щ", "ЮД_Е"},
+	//		{"Ы_ЩБ", "Ж_БЙ", "Ф_ЕЮ", "Ы_ЩБ", "Л_ЗЗ"},
+	//		{"Ы_ЩБ", "Ж_БЙ", "Ф_ЕЮ", "У_ЧЧ", "Д_ЕЮ"},
+	//		{"Ы_ЩБ", "Ж_БЙ", "Ь_ЗЗ", "Ы_ЩБ", "Д_ЕЮ"},
+	//		{"Ы_ЩБ", "____", "Ф_ЕЮ", "Ы_ЩБ", "Д_ЕЮ"},
+	//	}
+	//)
 
-	alphabet := *l1.NewAlphabet(TelegraphAlphabet)
-	sponge := l2.NewSponge(InnerState, alphabet)
+	//sponge := l2.NewSponge(InnerState, alphabet)
 
-	var (
-		IN  = "ХОРОШО_БЫТЬ_ВАМИ"
-		IN1 = "КЬЕРГЕГОР_ПРОПАЛ"
-		IN2 = "ХОРОШО_ПРОБРОСИЛ"
-	)
-	fmt.Println(sponge.Confuse(IN, IN1))
-	fmt.Println(sponge.Confuse(IN, IN2))
-	fmt.Println(sponge.Confuse(IN, IN))
+	//var (
+	//	IN  = "ХОРОШО_БЫТЬ_ВАМИ"
+	//	IN1 = "КЬЕРГЕГОР_ПРОПАЛ"
+	//	IN2 = "ХОРОШО_ПРОБРОСИЛ"
+	//)
+	//fmt.Println(sponge.Confuse(IN, IN1))
+	//fmt.Println(sponge.Confuse(IN, IN2))
+	//fmt.Println(sponge.Confuse(IN, IN))
 
 	//var (
 	//	IN1 = []string{"ХОРОШО_БЫТЬ_ВАМИ"}
@@ -46,4 +58,51 @@ func main() {
 	//fmt.Println(l2.NewSponge(InnerState, alphabet).CBlock(IN4, 16))
 	//fmt.Println(l2.NewSponge(InnerState, alphabet).CBlock(IN4, 8))
 	//fmt.Println(l2.NewSponge(InnerState, alphabet).CBlock(IN4, 4))
+
+	//sponge := l2.NewSponge(state0, alphabet)
+	//Display(state0)
+	//state11 := sponge.MixCols()
+	//Display(state11)
+	//state12 := sponge.ShatterBlocks()
+	//Display(state12)
+	//state13 := sponge.ShiftRows()
+	//Display(state13)
+	//
+	//state21 := sponge.MixCols()
+	//Display(state21)
+	//state22 := sponge.ShatterBlocks()
+	//Display(state22)
+	//state23 := sponge.ShiftRows()
+	//Display(state23)
+	//
+	//state31 := sponge.MixCols()
+	//Display(state31)
+	//state32 := sponge.ShatterBlocks()
+	//Display(state32)
+	//state33 := sponge.ShiftRows()
+	//Display(state33)
+
+	//sponge := l2.NewSponge(state0, alphabet)
+	//sponge.SpongeAbsorb("_А__")
+	//sponge.SpongeAbsorb("ВИЛЯ")
+	//sponge.SpongeAbsorb("ОЗЛ_")
+	//sponge.SpongeAbsorb("ОЗЛ_")
+	//Display(sponge.InnerState)
+	//
+	//spongeX := l2.NewSponge(stateX, alphabet)
+	//out1 := spongeX.SpongeSqueeze()
+	//out2 := spongeX.SpongeSqueeze()
+	//out3 := spongeX.SpongeSqueeze()
+	//fmt.Println(out1, out2, out3)
+	//Display(spongeX.InnerState)
+
+	fmt.Println(l2.SpongeHash("КАТЕГОРИЧЕСКИЙ_ИМПЕРАТИВ", alphabet))
+	fmt.Println(l2.SpongeHash("________________________________________________________________", alphabet))
+}
+
+func Display(inp [5][5]string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(inp[i])
+	}
+	fmt.Println("")
 }
